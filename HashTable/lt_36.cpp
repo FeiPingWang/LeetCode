d38294a720e178814728c63103abbbcf25cb9d0a
FeiPingWang <wfp19920821@163.com>
2016-06-24 05:51:18 +0800

    updata new methon

--- HashTable/lt_36.cpp
+++ HashTable/lt_36.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+	//28ms using hashtable
     bool isValidSudoku(vector<vector<char>>& board) {
         if(board.size() == 0)
             return false;
@@ -39,6 +40,35 @@ public:
                 if(find(vu[n].begin(),vu[n].end(),board[i][j]) != vu[n].end()|| board[i][j] < '0' || board[i][j] > '9')
                     return false;
                 vu[n].push_back(board[i][j]);
+            }
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+//14ms,use bit manipulation instead of hashtable
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<int> row(9,0);
+        vector<int> col(9,0);
+        vector<int> matrix(9,0);
+        if(board.size() == 0)
+            return false;
+        for(int r = 0;r < board.size();r++)
+        {
+            for(int c = 0;c<board[0].size();c++)
+            {
+                char tmp = board[r][c];
+                if(tmp == '.')
+                    continue;
+                int digit = tmp-'.';
+                int mask = 1 << digit;
+                if(row[r] & mask || col[c] & mask || matrix[r/3*3+c/3] & mask)
+                    return false;
+                row[r] |= mask;
+                col[c] |= mask;
+                matrix[r/3*3+c/3] |= mask;
             }
         }
         return true;
